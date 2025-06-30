eedb14fcbff73b64f6563423ed57d68a5bdf53cc
kerro16 <kerrox16@gmail.com>
2025-06-30 16:51:45 -0600

    Primer commit

---
+++ src/main/java/com/kerro16/InventarioCasaBackend/InventarioCasaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kerro16.InventarioCasaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioCasaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioCasaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kerro16/InventarioCasaBackend/InventarioCasaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerro16.InventarioCasaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioCasaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
